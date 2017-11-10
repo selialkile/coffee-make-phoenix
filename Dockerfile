2bb0e8f195b434c08392cd53a8d88929a4656877
Thiago Coutinho <thiago.coutinho@locaweb.com.br>
2017-11-10 16:38:45 -0200

    ignore node_modules

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM elixir:1.5-alpine
 
-RUN apk update && apk add vim
+RUN apk update && apk add vim git bash nodejs nodejs-npm
 
 ADD docker/start_services.sh /root/start_services.sh
--- docker/start_services.sh
+++ docker/start_services.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-cd /var/www/coffee-maker
-tail -f *
+cd /var/www/coffee-maker/assets && npm install
 
+cd /var/www/coffee-maker
+
+# mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
 mix local.hex --force
 mix local.rebar --force
 mix deps.get
+
+tail -f *
